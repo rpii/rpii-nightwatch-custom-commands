d0a78a169ecf43ec80126587f74fecb2d6d9eb60
rich.peters <rpeters@micro-magic.com>
2019-03-05 18:05:37 -0800

    cleanup path issue with screenshot

--- js/commands/takeScreenshot.js
+++ js/commands/takeScreenshot.js
@@ -3,13 +3,6 @@ const events = require('events');
 const spath = process.cwd() + '/node_modules/nightwatch/lib/testsuite/screenshots.js'
 const Screenshots = require(spath);
 
-/*
- * This custom command allows us to locate an HTML element on the page and then wait until the element is both visible
- * and does not have a "disabled" state.  It rechecks the element state every 500ms until either it evaluates to true or
- * it reaches maxTimeInMilliseconds (which fails the test). Nightwatch uses the Node.js EventEmitter pattern to handle
- * asynchronous code so this command is also an EventEmitter.
- */
-
 function TakeScreenshot() {
     events.EventEmitter.call(this);
     this.startTimeInMilliseconds = null;
@@ -19,13 +12,12 @@ util.inherits(TakeScreenshot, events.EventEmitter);
 
 TakeScreenshot.prototype.command = function(logMessage, filename, callback = function() {}) {
     var self = this;
-    const fileName = self.api.globals.screenshotPath + filename;
     self.api.screenshot(true, result => {
         return new Promise((resolve, reject) => {
             //gets the assert in the log for picture to append to
             self.api.assert.ok(true, logMessage);
 
-            Screenshots.writeScreenshotToFile(fileName, result.value, err => {
+            Screenshots.writeScreenshotToFile(filename, result.value, err => {
                 const cbResult = callback.call(this, result, err);
                 if (cbResult instanceof Promise) {
                     cbResult.then(_ => resolve(result)).catch(ex => reject(ex));
@@ -34,7 +26,7 @@ TakeScreenshot.prototype.command = function(logMessage, filename, callback = fun
                     const assertions = self.api.currentTest.results.assertions || [];
                     let currentAssertion = assertions[assertions.length-1];
                     currentAssertion.screenshots = currentAssertion.screenshots || [];
-                    currentAssertion.screenshots.push(fileName);
+                    currentAssertion.screenshots.push(filename);
                     resolve(result);
                     self.emit('complete');
                 }
